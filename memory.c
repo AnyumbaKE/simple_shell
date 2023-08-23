bd6b35e7347b5e7c181db41ee5667b08951683d5
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 15:01:08 -0700

    memory

---
+++ memory.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * free_all - frees all malloc'd space at end of main loop
+ * @tokens: pointer to tokens array
+ * @path: pointer to path variable
+ * @line: pointer to user input buffer
+ * @fullpath: pointer to full path
+ * @flag: flag marking if full_path was malloc'd
+ * Return: void
+ */
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
+{
+	free(path);
+	free(tokens);
+	free(line);
+	if (flag == 1)
+		free(fullpath);
+}
+
+/**
+ * free_dp - free double pointer
+ * @array: double pointer to free
+ * @length: length of double pointer
+ * Return: void
+ */
+void free_dp(char **array, unsigned int length)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < length)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
