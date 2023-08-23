7bcc1d112108b229f8e6c8f9accb5106d354e29d
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 14:51:45 -0700

    shellenv

---
+++ shell_env.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+**shell_env - prints environment
+**Return: void
+**/
+
+	int shell_env(void)
+	{
+		unsigned int i;
+
+		i = 0;
+		while (environ[i] != NULL)
+		{
+			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+			i++;
+		}
+		return (0);
+	}
