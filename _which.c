e15ed3d18494db08bf271fa7a2f1a64e6c323718
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 14:08:41 -0700

    which

---
+++ _which.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+/**
+ * _which - search directories in PATH variable for command
+ * @command: to search for
+ * @fullpath: full path of command to execute
+ * @path: full PATH variable
+ * Return: pointer to full_path
+ */
+char *_which(char *command, char *fullpath, char *path)
+{
+	unsigned int command_length, path_length, original_path_length;
+	char *path_copy, *token;
+
+	command_length = _strlen(command);
+	original_path_length = _strlen(path);
+	path_copy = malloc(sizeof(char) * original_path_length + 1);
+	if (path_copy == NULL)
+	{
+		errors(3);
+		return (NULL);
+	}
+	_strcpy(path_copy, path);
+
+	token = strtok(path_copy, ":");
+	if (token == NULL)
+		token = strtok(NULL, ":");
+	while (token != NULL)
+	{
+		path_length = _strlen(token);
+		fullpath = malloc(sizeof(char) * (path_length + command_length) + 2);
+		if (fullpath == NULL)
+		{
+			errors(3);
+			return (NULL);
+		}
+		_strcpy(fullpath, token);
+		fullpath[path_length] = '/';
+		_strcpy(fullpath + path_length + 1, command);
+		fullpath[path_length + command_length + 1] = '\0';
+		if (access(fullpath, X_OK) != 0)
+		{
+			free(fullpath);
+			fullpath = NULL;
+			token = strtok(NULL, ":");
+		}
+		else
+			break;
+	}
+	free(path_copy);
+	return (fullpath);
+}
