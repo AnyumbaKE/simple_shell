9fe78a5bfcc0f9133f72663b8773ace8a4518896
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 12:38:24 -0700

    environment

---
+++ environment.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+/**
+ * _getenv - gets an environment variable
+ * @name: environment variable to get
+ * Return: pointer to environment variable or NULL if there is no match
+ */
+char *_getenv(const char *name)
+{
+	char **environ_copy;
+	char *variable, *value, *path;
+	int compare;
+	unsigned int path_length, environ_length, length, i;
+
+	environ_length = 0;
+	while (environ[environ_length] != NULL)
+		environ_length++;
+	environ_copy = NULL;
+	environ_copy = copy_env(environ_copy, environ_length);
+
+	length = _strlen((char *)name);
+	i = 0;
+	while (environ_copy[i] != NULL)
+	{
+		variable = environ_copy[i];
+		compare = _strncmp((char *)name, variable, length);
+		if (compare == 1)
+		{
+			value = strtok(variable, "=");
+			value = strtok(NULL, "\n ");
+			if (value == '\0')
+			{
+				errors(4);
+				exit(EXIT_FAILURE);
+			}
+			path_length = _strlen(value);
+			path = malloc(sizeof(char) * path_length + 1);
+			if (path == NULL)
+			{
+				errors(3);
+				return (NULL);
+			}
+			path = _strcpy(path, value);
+			free_dp(environ_copy, environ_length);
+			return (path);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * copy_env - copies environment variable
+ * @environ_copy: pointer to copy of environment variable
+ * @environ_length: length of environment variable
+ * Return: double pointer to copy of environment variable
+ */
+char **copy_env(char **environ_copy, unsigned int environ_length)
+{
+	char *variable;
+	unsigned int variable_length;
+	unsigned int i;
+
+	environ_copy = malloc(sizeof(char **) * (environ_length));
+	if (environ_copy == NULL)
+	{
+		errors(3);
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < environ_length)
+	{
+		variable = environ[i];
+		variable_length = _strlen(variable);
+
+		environ_copy[i] = malloc(sizeof(char) * variable_length + 1);
+		if (environ_copy[i] == NULL)
+		{
+			errors(3);
+			return (NULL);
+		}
+		_strcpy(environ_copy[i], environ[i]);
+		i++;
+	}
+
+	return (environ_copy);
+}
