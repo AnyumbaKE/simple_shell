5d1c9fd79396bf34b3ff117bbcc3ba3e1003387a
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 11:31:27 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,75 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER 1024
+#define TRUE 1
+#define PROMPT "$ "
+
+/* error strings */
+#define ERR_MALLOC
+#define ERR_FORK
+#define ERR_PATH
+extern char **environ;
+
+/**
+ * struct list_s - linked list of variables
+ * @value: value
+ * @next: pointer to next node
+ * Description: generic linked list struct for variables.
+**/
+typedef struct list_s
+{
+	char *value;
+	struct list_s *next;
+} list_s;
+
+/**
+ * struct built_s - linked list of builtins
+ * @name: name of builtin
+ * @p: pointer to function
+ * Description: struct for builtin functions.
+**/
+typedef struct built_s
+{
+	char *name;
+	int (*p)(void);
+} built_s;
+
+void prompt(int fd, struct stat buf);
+char *_getline(FILE *fp);
+char **tokenizer(char *str);
+char *_which(char *command, char *fullpath, char *path);
+int child(char *fullpath, char **tokens);
+void errors(int error);
+
+/* utility functions */
+void _puts(char *str);
+int _strlen(char *s);
+int _strcmp(char *name, char *variable, unsigned int length);
+int _strncmp(char *name, char *variable, unsigned int length);
+char *_strcpy(char *dest, char *src);
+
+/* free functions */
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
+void free_dp(char **array, unsigned int length);
+
+/* prototypes for builtins */
+int shell_env(void);
+int shell_exit(void);
+int builtin_execute(char **tokens);
+int shell_num_builtins(built_s builtin[]);
+
+/*  helper functions for path linked list */
+char *_getenv(const char *name);
+char **copy_env(char **environ_copy, unsigned int environ_length);
+list_s *pathlist(char *variable, list_s *head);
+
+#endif 
