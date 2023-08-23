c09b96bb8e7739985f9834a05ee5dce074728114
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 14:33:29 -0700

    child

---
+++ child.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * child - child process
+ * @fullpath: full path of executable
+ * @tokens: tokenized user input
+ * Return: 0 on success
+ */
+int child(char *fullpath, char **tokens)
+{
+	pid_t child_pid;
+	int status;
+	int execve_status;
+	char **envp = environ;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		errors(1);
+		exit(EXIT_FAILURE);
+	}
+	if (child_pid == 0)
+	{
+		execve_status = execve(fullpath, tokens, envp);
+		if (execve_status == -1)
+			return (-1);
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
