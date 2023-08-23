f1f9f2d84e66ff7ce3a38e2264552159684eed81
AnyumbaKE <esanyumba@gmail.com>
2023-08-23 15:11:37 -0700

    token

---
+++ tokenizer.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * tokenizer - tokenizes string
+ * @str: user input
+ * Return: pointer to array of tokens
+ */
+char **tokenizer(char *str)
+{
+	char **tokens;
+	char *token;
+	unsigned int i;
+
+	tokens = malloc(sizeof(char) * BUFFER);
+	if (tokens == NULL)
+	{
+		errors(3);
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(str, "\n\t\r ");
+
+	i = 0;
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, "\n\t\r ");
+		i++;
+	}
+
+	tokens[i] = NULL;
+
+	return (tokens);
+}
