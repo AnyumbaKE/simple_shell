8bc3fbb55c37bbe10347e40c6fd328a31e439924
AnyumbaKE <esanyumba@gmail.com>
2023-08-24 19:38:04 -0700

    lists

--- lists.c
+++ lists.c
@@ -1,5 +1,27 @@
 #include "shell.h"
 /**
+ * free_line_list - frees a line_list
+ * @head: head of the linked list
+ * Return: 0 Always
+ **/
+void free_line_list(line_list **head)
+{
+	line_list *temp;
+	line_list *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * add_rvar_node - adds a variable at the end
  * @head: head of the linked list
  * @lvar: length of the variable
