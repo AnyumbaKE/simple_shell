e174287c3c3f473aa849a689036d1ecb4f7ebb67
QueenMaggie <wanjirumargaret394@gmail.com>
2023-08-25 07:28:33 -0700

    lineExecutor.c

--- lineExecutor.c
+++ lineExecutor.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * exec_line - finds builtins and commands
+ * exec_line - function that finds builtins and commands
  * @datash: data relevant (args)
  * Return: 1 on success.
  */
