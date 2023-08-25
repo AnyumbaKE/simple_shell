af4fd78e30bb96a78a614f3828dd75a82dff43d9
QueenMaggie <wanjirumargaret394@gmail.com>
2023-08-25 08:30:53 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -102,7 +102,7 @@ void free_line_list(line_list **head);
 r_var *add_rvar_node(r_var **head, int lvar, char *var, int lval);
 void free_rvar_list(r_var **head);
 
-/* memory functions */
+/* memory.c functions */
 void _memcpy(void *newptr, const void *ptr, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
